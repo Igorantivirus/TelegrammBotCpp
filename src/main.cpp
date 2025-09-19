d539998731c6cfdd4c648276346b1a0c500e36ec
Игорь Антипов <igorantivirus@yandex.ru>
2025-09-19 22:16:09 +0700

    Убран старый код

--- src/main.cpp
+++ src/main.cpp
@@ -1,101 +1,23 @@
 #define _SILENCE_ALL_CXX17_DEPRECATION_WARNINGS
 
-#include"MyApiKey.hpp"
+#include "MyApiKey.hpp"
 
-#include"BotResponser.hpp"
+#include"Bot.hpp"
 
-void COnsoleGadalka()
-{
-    Responser resp;
+// void COnsoleGadalka()
+// {
+//     Responser resp;
 
-    std::string s;
+//     std::string s;
 
-    bool agr = false;
+//     bool agr = false;
 
-    while (true)
-    {
-        std::getline(std::cin, s);
-        std::cout << resp.GetAnswer(s, agr) << std::endl;
-    }
-}
-
-
-class GadalkaBot
-{
-public:
-    GadalkaBot(const char* key) : bot(key), processor(key)
-    {
-        InitResponses();
-    }
-
-    void Run()
-    {
-        try
-        {
-            std::cout << "Bot username: " << bot.getApi().getMe()->username.c_str() << '\n';
-            TgBot::TgLongPoll longPoll(bot);
-            while (true)
-            {
-                std::cout << "Long poll started" << '\n';
-                longPoll.start();
-            }
-        }
-        catch (const TgBot::TgException& e)
-        {
-            std::cout << "Bot error: " << e.what() << '\n';
-        }
-        catch (...)
-        {
-            std::cout << "Unknown Error" << '\n';
-        }
-    }
-    
-private:
-
-    TgBot::Bot bot;
-
-    InputProcessor processor;
-
-    void InitResponses()
-    {
-        bot.getEvents().onAnyMessage([this](TgBot::Message::Ptr message)
-            {
-                std::string res;
-                try
-                {
-                    res = processor.Processing(message->text, message);
-                    bot.getApi().sendMessage(message->chat->id, res);
-                }
-                catch (...)
-                {
-                    std::cout << "Error: " << "Trying send message \"" << res << "\"\n";
-                    bot.getApi().sendMessage(message->chat->id, to_utf8(L"Ошибка!"));
-                }
-            });
-        bot.getEvents().onInlineQuery([this](const TgBot::InlineQuery::Ptr& query)
-            {
-                std::string res = processor.ProcessingLineMessage(query);
-                if (res.empty())
-                    return;
-
-                std::vector<TgBot::InlineQueryResult::Ptr> results;
-                TgBot::InlineQueryResultArticle::Ptr article = std::make_shared<TgBot::InlineQueryResultArticle>();
-
-                article->title = "Math result.";
-                article->id = "1";
-
-                TgBot::InputTextMessageContent::Ptr messageContent = std::make_shared<TgBot::InputTextMessageContent>();
-                messageContent->messageText = res;
-                article->inputMessageContent = messageContent;
-
-                results.push_back(article);
-                bot.getApi().answerInlineQuery(query->id, results);
-
-
-            });
-    }
-
-};
+//     while (true)
+//     {
+//         std::getline(std::cin, s);
+//         std::cout << resp.GetAnswer(s, agr) << std::endl;
+//     }
+// }
 
 const std::string TgBot::InlineQueryResultArticle::TYPE = "article";
 const std::string TgBot::InputTextMessageContent::TYPE = "text";
@@ -106,9 +28,8 @@ int main()
     system("chcp 65001 > nul");
 #endif
 
-    GadalkaBot bot(TG_API_KEY);
-    bot.Run();
-
+    Bot bot(TG_API_KEY);
+    bot.run();
 
     return 0;
 }
