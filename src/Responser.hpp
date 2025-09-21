1704d6f07ab20dc530b7c0ea8914552ddf6ead7f
Игорь Антипов <igorantivirus@yandex.ru>
2025-09-22 00:15:27 +0700

    Добавлена обработка incline запросов бота для анекдотов

--- src/Responser.hpp
+++ src/Responser.hpp
@@ -6,26 +6,31 @@
 #include <tgbot/tgbot.h>
 
 #include "MathSolver.hpp"
+#include "Anekdots/AnekdotGenerator.hpp"
 
 class Responser
 {
 public:
+    Responser() :
+        generator_("assets/PagesData.xml")
+    {}
 
-
-    std::pair<std::string, bool> getResponsesOnInlineMessages(const TgBot::InlineQuery::Ptr& query)
+    std::pair<std::string, bool> getResponsesOnInlineMessages(const TgBot::InlineQuery::Ptr &query)
     {
-        if(solver_.isInlineMathQuestion(query->query))
+        if (solver_.isInlineMathQuestion(query->query))
             return solver_.getMathInlineResponseResult(query->query);
+        std::string request = query->query;
+        StringUtility::toLower(request);
+
+        if (generator_.isInlineAnekRequest(request))
+            return {generator_.parseAnekdotAtKeyword(request), true};
+
         return {"", false};
-        
     }
 
 private:
-
     MathSolver solver_;
+    AnekdotGenerator generator_;
 
 private:
-
-
-
 };
